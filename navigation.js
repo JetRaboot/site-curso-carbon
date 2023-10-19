50ec0cb276cc2ca21b086b336b238fc29e432ece
JetRaboot <musga37@gmail.com>
2023-10-19 16:53:53 -0300

    site

---
+++ navigation.js
@@ -0,0 +1,11 @@
+document.getElementById("m1").addEventListener("click", function() {
+    window.location.href = "m1.html";
+});
+
+document.getElementById("m2").addEventListener("click", function() {
+    window.location.href = "m2.html";
+});
+
+document.getElementById("m3").addEventListener("click", function() {
+    window.location.href = "m3";
+});
